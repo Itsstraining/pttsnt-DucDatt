e610ac9de611d6d78e6b50c422b10297e0034cbd
DucDatt <dat.hd3745@sinhvien.hoasen.edu.vn>
2022-02-19 10:45:16 +0700

    Duc Dat

--- program.js
+++ program.js
@@ -1,8 +1,17 @@
 // Add new functions, variables here
 
 function main(input) {
-  // Your code goes here
-  // Using console.log to answer the question
+    // Your code goes here
+    // Using console.log to answer the question
+    for (let i = 2; i <= input; i++) {
+        while (input % i == 0) {
+            console.log(i);
+            input /= i;
+        }
+    }
 }
+main(100);
+
+
 
 module.exports = main;
